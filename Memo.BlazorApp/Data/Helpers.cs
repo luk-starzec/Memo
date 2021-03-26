ce7760cbbd711d0b73ece3368b7a8b371c370a63
Łukasz Starzec <luk.starzec@gmail.com>
2021-03-26 21:17:32 +0100

    Wstępna struktura html

---
+++ Memo.BlazorApp/Data/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Memo.BlazorApp.Data
+{
+    public static class Helpers
+    {
+        public static string GetUrl(string title) => title?.Replace(" ", "-").Trim();
+
+    }
+}
